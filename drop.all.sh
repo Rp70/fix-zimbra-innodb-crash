154782a316e406b7550d0ab1129b4b24858ae239
Rp70 <lequocdung99vn@yahoo.com>
2023-07-06 19:00:16 +0000

    refactor: reduce waiting time for warning message to 9 secs (was 45 secs)

--- drop.all.sh
+++ drop.all.sh
@@ -2,8 +2,8 @@
 
 source ./_include.all.sh
 
-echo "This will sleep for 45 seconds before start dropping all databases. If you want to stop it, cancel it now by pressing Ctrl + c"
-sleep 45
+echo "This will sleep for 9 seconds before start dropping all databases. If you want to stop it, cancel it now by pressing Ctrl + c"
+sleep 9
 
 echo "Droping all databases"
 for db in `cat /tmp/mysql.db.list | grep mbox`
